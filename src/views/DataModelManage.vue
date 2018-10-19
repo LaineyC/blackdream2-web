49b1e2dca6168b601e2cf8a67beb5a799e40bcd5
LaineyC <272874065@qq.com>
2018-10-19 20:05:16 +0800

    图标

--- src/views/DataModelManage.vue
+++ src/views/DataModelManage.vue
@@ -35,9 +35,9 @@
                                 <el-input v-model="item.model.name" placeholder="" />
                             </el-form-item>
                             <el-form-item label="图标" prop="iconStyle">
-                                <el-popover placement="bottom" width="400" trigger="click">
-                                    <el-button :icon="icon"  v-for="icon in iconStyleList" :key="icon" @click="item.model.iconStyle=icon"></el-button>
-                                    <el-button slot="reference"> <i :class="item.model.iconStyle"></i></el-button>
+                                <el-popover placement="bottom" width="350" trigger="click">
+                                    <el-button style="margin: 5px;font-size: 24px;padding: 2px;" :icon="icon"  v-for="icon in iconStyleList" :key="icon" @click="item.model.iconStyle=icon" plain></el-button>
+                                    <el-button slot="reference" style="font-size: 24px;padding: 2px;"> <i :class="item.model.iconStyle"></i></el-button>
                                 </el-popover>
                             </el-form-item>
                             <el-card shadow="hover">
@@ -321,7 +321,20 @@
                     children: 'children',
                     label: 'name'
                 },
-                iconStyleList:["el-icon-star-on","el-icon-location", "el-icon-tickets", "el-icon-bell","el-icon-picture"],
+                iconStyleList:[
+                    "el-icon-info","el-icon-error", "el-icon-success", "el-icon-warning","el-icon-question", "el-icon-back",
+                    "el-icon-arrow-left", "el-icon-arrow-down", "el-icon-arrow-right", "el-icon-arrow-up", "el-icon-caret-left", "el-icon-caret-bottom",
+                    "el-icon-caret-top", "el-icon-caret-right", "el-icon-d-arrow-left", "el-icon-d-arrow-right", "el-icon-minus", "el-icon-plus",
+                    "el-icon-remove", "el-icon-circle-plus", "el-icon-remove-outline", "el-icon-circle-plus-outline", "el-icon-close", "el-icon-check",
+                    "el-icon-circle-close", "el-icon-circle-check", "el-icon-circle-close-outline", "el-icon-circle-check-outline", "el-icon-zoom-out", "el-icon-zoom-in",
+                    "el-icon-d-caret", "el-icon-sort", "el-icon-sort-down", "el-icon-sort-up", "el-icon-tickets", "el-icon-document",
+                    "el-icon-goods", "el-icon-sold-out", "el-icon-news", "el-icon-message", "el-icon-date", "el-icon-printer",
+                    "el-icon-time", "el-icon-bell", "el-icon-mobile-phone", "el-icon-service", "el-icon-view", "el-icon-menu",
+                    "el-icon-more", "el-icon-more-outline", "el-icon-star-on", "el-icon-star-off", "el-icon-location", "el-icon-location-outline",
+                    "el-icon-phone", "el-icon-phone-outline", "el-icon-picture", "el-icon-picture-outline", "el-icon-delete", "el-icon-search",
+                    "el-icon-edit", "el-icon-edit-outline", "el-icon-rank", "el-icon-refresh", "el-icon-share", "el-icon-setting",
+                    "el-icon-upload", "el-icon-upload2", "el-icon-download", "el-icon-loading",
+                ],
                 validRule: {
                     name: [
                         { required: true, message: '请填写名称', trigger: 'blur' },
@@ -385,7 +398,7 @@
                     id: "$" + id,
                     name: "新建模型" + id,
                     generatorId: this.generatorId,
-                    iconStyle: "el-icon-star-on",
+                    iconStyle: "el-icon-menu",
                     propertyList: [{
                         id:this.Method.generateId(),
                         name:"name",
